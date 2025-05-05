cd81d3090c729436ad04337b93cb76f72dfb9aa8
shivu1919 <shivamthakur.ss129@gmail.com>
2025-05-05 14:58:32 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Login from '../src/pages/Login'
 import Signup from '../src/pages/Signup'
 import Home from '../src/pages/Home'
-
+import Protected from './pages/Protected'
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Login/>}></Route>
           <Route path="/register" element={<Signup/>}></Route>
-          <Route path='/home' element={<Home/>}></Route>
+          <Route path='/home' element={<Protected Component={Home}/>}></Route>
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { signOut } from 'firebase/auth';
+import {auth} from '../Firebase'
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+
+  const navigate = useNavigate()
+
+  const logOutUser = () =>{
+    signOut(auth).then(() => {
+      navigate("/")
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
+    });
+  }
+
   return (
-    <div>Home</div>
+    <>
+      <h1>Home page</h1>
+      <button onClick={logOutUser}>Log out</button>
+    </>
   )
 }
 
